f9a87eb8ab150d27dbccb0d4af52e7d382744636
John Carter <jcarter345@gmail.com>
2022-07-19 22:23:30 -0500

    Changed tocancel button in XML added load time.
    Changed the Close button to cancel in the XML definition to take care of closing function.
    
    added on load call to show time to start.

--- main.ps1
+++ main.ps1
@@ -9,7 +9,7 @@ $Xaml = @"
 <Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation" Width="800" Height="400">
 <Grid>
  <Button Content="Update Time" HorizontalAlignment="Left" VerticalAlignment="Top" Width="75" Margin="292,186,0,0" Name="UpdateDisplay"/>
-<Button Content="Close App" HorizontalAlignment="Left" VerticalAlignment="Top" Width="75" Margin="292,225,0,0" Name="CloseApp"/>
+<Button Content="Close App" HorizontalAlignment="Left" VerticalAlignment="Top" Width="75" Margin="292,225,0,0" Name="CloseApp"  IsCancel="True"/>
 <Label VerticalAlignment="Top" Content="Label" Margin="292,123,0,0" Name="TimeDisplay"/>
 </Grid></Window>
 "@
@@ -20,7 +20,14 @@ $Xaml = @"
 
 
 #region Logic
-#Write your code here
+Function Update-TimeDisplay {
+    #$TimeDisplay.Content = (get-date -Format "HH:mm:ss")
+    return (get-date -Format "HH:mm:ss")
+}
+
+function Close-TimeApp {
+        #$window
+}
 #endregion 
 
 
@@ -35,8 +42,10 @@ $Window = [Windows.Markup.XamlReader]::Parse($Xaml)
 $xml.SelectNodes("//*[@Name]") | ForEach-Object { Set-Variable -Name $_.Name -Value $Window.FindName($_.Name) }
 
 
-$UpdateDisplay.Add_Click({Update-TimeDisplay $this $_})
-$CloseApp.Add_Click({Close-TimeApp $this $_})
+$UpdateDisplay.Add_Click({$TimeDisplay.Content = (Update-TimeDisplay $this $_)})
+
+#update time on first run.
+$TimeDisplay.Content = Update-TimeDisplay
 
 $Window.ShowDialog()
 
